47fc42cb9e622a6496411f5f4063c4b871cda832
Atili0 <atilio.rosas.e@gmail.com>
2018-10-05 16:01:16 +0200

    NUEVO CLASE DE PLUGIN AUTONOMERICO

--- dlabs.Autonumber.Autonumber/POST_CRE_Set_Number.cs
+++ dlabs.Autonumber.Autonumber/POST_CRE_Set_Number.cs
@@ -1,31 +1,24 @@
-﻿using DexLabs_Trace;
-using Microsoft.Xrm.Sdk;
+﻿using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
 using System;
 
-namespace Dlabs.Autonumber
+namespace Dxrm.Autonumber
 {
-    public class POST_CRE_dx_confignumber : IPlugin
+    public class POST_CRE_Set_Number : IPlugin
     {
         #region Secure/Unsecure Configuration Setup
         private string _secureConfig = null;
         private string _unsecureConfig = null;
 
-        public POST_CRE_dx_confignumber(string unsecureConfig, string secureConfig)
+        public POST_CRE_Set_Number(string unsecureConfig, string secureConfig)
         {
             _secureConfig = secureConfig;
             _unsecureConfig = unsecureConfig;
         }
         #endregion
-
-
-        private ITrace _trace;
-
         public void Execute(IServiceProvider serviceProvider)
         {
-            
-
-        ITracingService tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            ITracingService tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
             IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
             IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
             IOrganizationService service = factory.CreateOrganizationService(context.UserId);
@@ -33,11 +26,8 @@ namespace Dlabs.Autonumber
             try
             {
                 Entity entity = (Entity)context.InputParameters["Target"];
-                _trace = new DxTrace(service, this.GetType().Name)
-                {
-                    TracingService = tracer
-                };
 
+                //TODO: Do stuff
             }
             catch (Exception e)
             {
